5890833ad549142db339a1b72d7e0ed46b14cd8f
Henri <henkkamoi02@gmail.com>
2023-11-20 12:26:49 +0200

    Initial concepting

---
+++ src/Contacteditor.java
@@ -0,0 +1,6 @@
+class Contacteditor {
+
+}
+
+//This is the class where the user would input and edit the contacts and will save
+//the information to a JSON file.
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void (String [] args) {
+
+    }
+}
+
+//The main hub of the code which will call all classes and methods and execute them for the user
+//Possible changes coming to the structure and potential expansions for classes when necessary
