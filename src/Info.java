4162d07b21033844e46160017ddf4609db7089de
Henri Nieminen <henkkamoi02@gmail.com>
2023-11-21 23:45:48 +0200

    Some syntax changes

--- src/Info.java
+++ src/Info.java
@@ -6,8 +6,10 @@ class Info {
     private String address;
     private String email;
 
-    
+    public void BornInTheUSA(String id, String phone,) {
+        System.out.println("Booorn in the U-S-A!");
+    }
 }
 
 //This is the class which contains the user given information and stores them to a file.
-//
+//Don't get spoiled by the Bruce Springstein reference. It's heavily a WIP.
